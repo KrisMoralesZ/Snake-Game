81d7af1f156f41612c4dd75a7f8f2631d4efd791
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2022-03-29 17:59:20 -0700

    movement test

--- test/actions_test.rb
+++ test/actions_test.rb
@@ -3,8 +3,9 @@ require_relative "../src/actions/actions"
 require_relative "../src/model/state"
 
 class ActionTest < Minitest::Test
-    def test_move_snake
-    initial_state = Model::State.new(
+
+    def setup
+        @initial_state = Model::State.new(
         Model::Snake.new([
             Model::Coord.new(1,1),
             Model::Coord.new(0,1)                
@@ -13,7 +14,10 @@ class ActionTest < Minitest::Test
         Model::Grid.new(8,12),
         Model::Direction::DOWN
     )
-    expected_state = initial_state = Model::State.new(
+    end
+
+    def test_move_snake
+    expected_state = Model::State.new(
         Model::Snake.new([
             Model::Coord.new(2,1),
             Model::Coord.new(1,1)                
@@ -22,8 +26,36 @@ class ActionTest < Minitest::Test
         Model::Grid.new(8,12),
         Model::Direction::DOWN
     )
-    actual_state = Actions::move_snake(initial_state)
+    actual_state = Actions::move_snake(@initial_state)
         assert_equal actual_state, 
         expected_state
     end
+
+    def test_change_direction_invalid
+        expected_state = Model::State.new(
+            Model::Snake.new([
+                Model::Coord.new(4, 4),
+                Model::Grid.new(8, 12)
+            ]),
+            Model::Food.new(4, 4),
+            Model::Grid.new(8, 12),
+            Model::Direction::DOWN
+        )
+        actual_state = Actions::change_direction(@initial_state, Model::Direction::UP)
+        assert_equal actual_state, expected_state
+    end
+
+    def test_change_direction_valid
+        expected_state = Model::State.new(
+            Model::Snake.new([
+                Model::Coord.new(4, 4),
+                Model::Grid.new(8, 12)
+            ]),
+            Model::Food.new(4, 4),
+            Model::Grid.new(8, 12),
+            Model::Direction::DOWN
+        )
+        actual_state = Actions::change_direction(@initial_state, Model::Direction::LEFT)
+        assert_equal actual_state, expected_state
+    end 
 end
